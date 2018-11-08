9f93a6229311588443b40afd92eca50c487ea8a4
WINMAC17\2daw <sergio_311096@hotmail.es>
2018-11-08 12:28:48 +0100

    subir test league

--- buggy_code/__tests__/test_league.js
+++ buggy_code/__tests__/test_league.js
@@ -1,17 +1,42 @@
 const League = require('../app/data/league').League;
-const Team = require('../app/data/team').Team;
+const Calendar = require('../app/data/league').Calendar;
+/*const Team = require('../app/data/team').Team;
 const readJSONData = require('../app/data/json_reader').readJSONData;
-const extractDataFromRawJSON = require('../app/data/json_reader').extractDataFromRawJSON;
+const extractDataFromRawJSON = require('../app/data/json_reader').extractDataFromRawJSON;*/
 
 //Testing construct
 test('Constructor error', () => {
-    expect(() => new League(0));
+    let league = new League(5);
+    expect(league.listTeams).toEqual([]);
+    expect(league.numberOfTeams).toBe(5);
+    expect(league.calendar).toBe(null);
+    expect(league.leagueTable).toBe(null); 
 });
 
-//testing league fromDatasetJSONObject method
+/*//Test addTeam in a League
+test('Test to add team in League', () => {
+
+    let league = new League("Liga1");
+    //let jsonObjects = readJSONData('./data/sample.json');
+    let team = new Team("Real Madrid");
+    //add team
+    expect(league.addTeam(team)).toBe(true);
+    
+})*/
+
+
+//Test createCalendar
+test('Test to create a Calendar', () => {
+
+    let calendar = new Calendar([]);
+    expect(calendar.createCalendar).toEqual([]);
+})
+
+
+/*testing league fromDatasetJSONObject method
 test('Testing league fromDatasetJSONObject method', () =>{
     // let pl = new League(0);
     let jsonObjects = readJSONData('./data/sample.json');
       let league = League.fromDatasetJSONObject(0,jsonObjects[0]);
       expect(league.getName()).toBe("");
-})
+})*/
